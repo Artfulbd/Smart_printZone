6db13c11883b99ffca970472940325a37b760b8c
Artfulbd <md.artful.bd@gmail.com>
2020-03-12 00:19:11 +0600

    v.0.01
    Very initial edition
    - Only Drop box functionaly added, with catching possible exception scenario.

---
+++ Client_app/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Smart_printZone_Client
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainForm));
+            this.panelDrop = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelDropIndicator = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.panelDrop.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelDrop
+            // 
+            this.panelDrop.AllowDrop = true;
+            this.panelDrop.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panelDrop.BackColor = System.Drawing.Color.SandyBrown;
+            this.panelDrop.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panelDrop.BackgroundImage")));
+            this.panelDrop.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.panelDrop.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelDrop.Controls.Add(this.label2);
+            this.panelDrop.Location = new System.Drawing.Point(1241, 623);
+            this.panelDrop.Name = "panelDrop";
+            this.panelDrop.Size = new System.Drawing.Size(153, 140);
+            this.panelDrop.TabIndex = 0;
+            this.panelDrop.DragDrop += new System.Windows.Forms.DragEventHandler(this.panelDrop_DragDrop);
+            this.panelDrop.DragEnter += new System.Windows.Forms.DragEventHandler(this.panelDrop_DragEnter);
+            this.panelDrop.MouseEnter += new System.EventHandler(this.panelDrop_MouseEnter);
+            this.panelDrop.MouseLeave += new System.EventHandler(this.panelDrop_MouseLeave);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(24, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "drop box";
+            // 
+            // labelDropIndicator
+            // 
+            this.labelDropIndicator.AutoSize = true;
+            this.labelDropIndicator.BackColor = System.Drawing.Color.Transparent;
+            this.labelDropIndicator.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDropIndicator.Location = new System.Drawing.Point(980, 733);
+            this.labelDropIndicator.Name = "labelDropIndicator";
+            this.labelDropIndicator.Size = new System.Drawing.Size(244, 26);
+            this.labelDropIndicator.TabIndex = 0;
+            this.labelDropIndicator.Text = "Drop your files here -->";
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.SystemColors.Info;
+            this.listBox1.Font = new System.Drawing.Font("Rockwell", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 37;
+            this.listBox1.Location = new System.Drawing.Point(721, 61);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(673, 374);
+            this.listBox1.TabIndex = 1;
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1406, 767);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.labelDropIndicator);
+            this.Controls.Add(this.panelDrop);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "mainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Smart Priner";
+            this.panelDrop.ResumeLayout(false);
+            this.panelDrop.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelDrop;
+        private System.Windows.Forms.Label labelDropIndicator;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Client_app/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Smart_printZone_Client
+{
+    public partial class mainForm : Form
+    {
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void panelDrop_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.All;
+        }
+
+        private void panelDrop_DragDrop(object sender, DragEventArgs e)
+        {
+            try
+            {
+                string[] droppedfiles = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+                if (droppedfiles.Length > 0)
+                {
+                    foreach (string hold in droppedfiles)
+                    {
+                        string fileName = getFileName(hold);
+                        listBox1.Items.Add(fileName);
+                        //listBox1.Items.Add(hold);
+                    }
+                }
+
+            }
+            catch (Exception catchedExcption)
+            {
+                listBox1.Items.Add("Are you fool or something");
+            }
+            finally
+            {
+                if (listBox1.Items.Count > 4)
+                {
+                    this.panelDrop.Enabled = false;
+                    this.panelDrop.BackColor = System.Drawing.Color.Red;
+                    this.labelDropIndicator.Text = "Reached to max limit 5";
+                }
+            }
+
+        }
+
+        private string getFileName(string path)
+        {
+            if (Directory.Exists(path)) return "This is not a file";
+            //else return Path.GetExtension(path);
+            else return Path.GetFileName(path);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void panelDrop_MouseLeave(object sender, EventArgs e)
+        {
+            this.panelDrop.BackColor = System.Drawing.Color.SandyBrown;
+        }
+
+        private void panelDrop_MouseEnter(object sender, EventArgs e)
+        {
+            this.panelDrop.BackColor = Color.Blue;
+        }
+    }
+}
