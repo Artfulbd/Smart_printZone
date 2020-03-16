8601193f96d112f791fcbf5e9103f6a49bbe4aea
Md. Ariful Haque <51986312+Artfulbd@users.noreply.github.com>
2020-03-16 16:51:04 +0600

    v 0.0.3
    FIle transfer using IP

---
+++ ConsoleTester/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //\\61V5TDL-SERVER\ServerFolder
+            //\\10.100.6.56\ServerFolder
+            
+            string serverName = "10.100.6.56";
+            string srcFolder = @"D:\Workspace\ConsoleTester\filesToShare";
+            string destFolder = @"\\"+serverName+@"\ServerFolder";
+            int dirLength = srcFolder.Length + 1;
+            bool isDone = false;
+
+
+            try
+            {
+                //if only specific file type is needed, here docx
+                //string[] fileList = Directory.GetFiles(srcFolder,"*.docx");
+                string[] fileList = Directory.GetFiles(srcFolder);
+
+                foreach(string singleFile in fileList)
+                {
+                    string fileName = singleFile.Substring(dirLength);
+                    Console.WriteLine(fileName);
+
+                    //overwrite file, if already exist
+                    File.Copy(Path.Combine(srcFolder, fileName), Path.Combine(destFolder, fileName), true);
+
+                    //doesn't overwrite if file already exist
+                    //File.Copy(Path.Combine(srcFolder, fileName), Path.Combine(destFolder, fileName));
+
+                    File.Delete(singleFile);
+                }
+
+
+                isDone = true;
+            }
+            catch(DirectoryNotFoundException drNotFound)
+            {
+                Console.WriteLine("Error.!! Dir not found, "+ drNotFound.Message);
+            }
+            if (isDone)
+            {
+                Console.WriteLine("File successfully copied");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
--- ConsoleTester/Properties/AssemblyInfo.cs
+++ ConsoleTester/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("IdeTest")]
+[assembly: AssemblyTitle("ConsoleTester")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("IdeTest")]
+[assembly: AssemblyProduct("ConsoleTester")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ec01116b-b10b-4f27-a311-9f322301843e")]
+[assembly: Guid("3b6dbf2e-50be-493e-982a-89c90c3196c9")]
 
 // Version information for an assembly consists of the following four values:
 //
