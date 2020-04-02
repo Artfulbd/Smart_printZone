400dc6261dbe421466a87875760e8862eb3ef9e3
Fahad Rahman Amik <56998203+Amik-TJ@users.noreply.github.com>
2020-04-02 23:50:00 +0600

    v 1.0.1
    Frontend structure Created for Admin Panel

---
+++ Front End/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>NSU Smart Print Zone</title>
+    <?php include 'link.php'; ?>
+</head>
+
+<body>
+<?php include 'nav_bar.php'; ?>
+
+</body>
+
+</html>
---
+++ Front End/link.php
@@ -0,0 +1,5 @@
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
